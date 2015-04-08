995ada3b774a04b3c65286dac0150d810aabd31e
dtrishkin <dtrishkin@mirantis.com>
2015-04-08 20:25:49 +0300

    In progress...

--- builder.py
+++ builder.py
@@ -17,9 +17,10 @@ grepTemplate0 = re.compile("import [a-zA-Z0-9-_.]+")
 grepTemplate1 = re.compile("from [a-zA-Z0-9-_.]+")
 sectTemplate = re.compile(":.+")
 
+dep_sects_list = ["Depends", "Build-Depends", "Build-Depends-Indep"]
 section_list = ["Source", "Section", "Priority", "Maintainer",
 "Build-Depends", "Build-Depends-Indep", "Standards-Version",
-"Homepage", "Package", "Depends"]
+"Homepage", "Package", "Depends", "Description"]
 section_dict = dict()
 
 def main():
@@ -42,14 +43,20 @@ def main():
 			section_dict["Maintainer"] = line["Maintainer"]
 			if line["Build-Depends"]:
 				section_dict["Build-Depends"] = line["Build-Depends"]
+				for el in section_dict["Build-Depends"].keys():
+					section_dict["Build-Depends"][el] = set(section_dict["Build-Depends"][el].items())
 			if line["Build-Depends-Indep"]:
 				section_dict["Build-Depends-Indep"] = line["Build-Depends-Indep"]
+				for el in section_dict["Build-Depends-Indep"].keys():
+					section_dict["Build-Depends-Indep"][el] = set(section_dict["Build-Depends-Indep"][el].items())
 			section_dict["Standards-Version"] = line["Standards-Version"]
 			section_dict["Homepage"] = line["Homepage"]
 			section_dict["Package"] = line["Package"]
 			section_dict["Architecture"] = line["Architecture"]
 			if line["Depends"]:
 				section_dict["Depends"] = line["Depends"]
+				for el in section_dict["Depends"].keys():
+					section_dict["Depends"][el] = set(section_dict["Depends"][el].items())
 			section_dict["Description"] = line["Description"]
 
 			load_control()
@@ -70,9 +77,11 @@ def main():
 			print get_dependencies(global_req, depends)
 			print get_build_dependencies(global_req, build_depends)
 			
-			for i in section_list:
-				if section_dict.has_key(i):
-					print "{0}{1}".format(i, section_dict[i])
+			for el in section_list:
+				if section_dict.has_key(el):
+					print "{0}{1}".format(el, section_dict[el])
+
+			build_control()
 		conf.close()
 
 	except KeyboardInterrupt:
@@ -89,9 +98,9 @@ def get_build_dependencies(global_req, build_depends, py_file_names = ["setup.py
 	build_depends = dict(build_depends.items() +
 		recur_search(names = py_file_names, search_type = "grep").items())
 
-	for i in excepts:
-		if build_depends.has_key(i):
-			del build_depends[i]
+	for el in excepts:
+		if build_depends.has_key(el):
+			del build_depends[el]
 	return build_depends
 
 def get_dependencies(global_req, depends, req_file_names = ["requires.txt", "requirements.txt"]):
@@ -106,15 +115,15 @@ def get_dependencies(global_req, depends, req_file_names = ["requires.txt", "req
 	return depends
 
 def recur_search(names, relative_path = ".", search_type = "default"):
-	for i in listdir(relative_path):
-		if isdir(join(relative_path, i)):
-			path = recur_search(names, join(relative_path, i), search_type)
+	for el in listdir(relative_path):
+		if isdir(join(relative_path, el)):
+			path = recur_search(names, join(relative_path, el), search_type)
 			if path:
 				return path
 		else:
-			if search_type == "grep" and i in names:
+			if search_type == "grep" and el in names:
 				try:
-					with open(join(relative_path, i),'r') as grep_file:
+					with open(join(relative_path, el),'r') as grep_file:
 						res_grep = dict()
 						for line in grep_file:
 							imp = grepTemplate0.search(line)
@@ -126,8 +135,8 @@ def recur_search(names, relative_path = ".", search_type = "default"):
 						return res_grep
 				except IOError:
 					None
-			elif i in names:
-				return join(relative_path, i)
+			elif el in names:
+				return join(relative_path, el)
 	return None
 
 def load_control(control_file_name = "control"):
@@ -135,7 +144,7 @@ def load_control(control_file_name = "control"):
 		with open(recur_search(control_file_name), 'r') as cont_file:
 			for line in cont_file:
 				for el in section_dict.keys():
-					if el not in ["Depends", "Build-Depends", "Build-Depends-Indep"] and el in line:
+					if el not in dep_sects_list and el in line:
 						if not section_dict[el]:
 							section_dict[el] = sectTemplate.search(line).group(0)
 	except IOError:
@@ -146,7 +155,19 @@ def build_control(control_file_name = "control"):
 		with open(recur_search(control_file_name), "w+") as control_file:
 			for el in section_list:
 				control_file.write(el)
-				
+				if el not in dep_sects_list:
+					control_file.write(section_dict[el] + "\n")
+				else:
+					print el
+					print section_dict[el]	
+					if section_dict[el]:
+						for key, val in section_dict[el].items():
+							control_file.write("\n")
+							for el2 in val:
+								control_file.write(" {0} ({1} {2}),".format(key, el2[0], el2[1]))
+					else:
+						control_file.write("\n")
+						control_file.write(" {0},".format(el))
 	except IOError:
 		print "Error while overwriting control file!"
 
