b9664d5324d6e61b014663320fb9030f9a8a5e27
dtrishkin <dtrishkin@mirantis.com>
2015-04-15 21:49:20 +0300

    Global refactoring according config.yaml

--- builder.py
+++ builder.py
@@ -19,11 +19,16 @@ fromTemplate = re.compile("from [a-zA-Z0-9-_.]+")
 sectTemplate = re.compile(":.+")
 build_depends = re.compile('"[a-zA-Z0-9-_.|<|>|=|!]+"')
 
-dep_sects_list = ["Depends", "Build-Depends", "Build-Depends-Indep",
-"Conflicts", "Provides", "Breaks", "Replaces", "Recommends", "Suggests"]
+build_dep_sects_list = ["Build-Depends", "Build-Depends-Indep"]
+dep_sects_list = ["Depends", "Conflicts", "Provides", "Breaks",
+"Replaces", "Recommends", "Suggests"]
 section_list = ["Source", "Section", "Priority", "Maintainer",
 "Build-Depends", "Build-Depends-Indep", "Standards-Version",
 "Homepage", "Package", "Architecture", "Depends", "Description"]
+main_section_list = ["Source", "Section", "Priority", "Maintainer",
+"Build-Depends", "Build-Depends-Indep", "Standards-Version", "Homepage"]
+package_section_list = ["Architecture", "Section", "Depends", "Conflicts",
+"Provides", "Breaks", "Replaces", "Recommends", "Suggests", "Description"]
 section_dict = dict()
 
 def main():
@@ -47,27 +52,35 @@ def main():
 			section_dict["Build-Depends"] = line["Build-Depends"]
 
 			if section_dict["Build-Depends"]:
-				for el in section_dict["Build-Depends"].keys():
-					section_dict["Build-Depends"][el] = \
-						{(el1.items()[0]) for el1 in section_dict["Build-Depends"][el]}
+				packages_processing(section_dict["Build-Depends"])
 
 			section_dict["Build-Depends-Indep"] = line["Build-Depends-Indep"]
 			if section_dict["Build-Depends-Indep"]:
-				for el in section_dict["Build-Depends-Indep"].keys():
-					section_dict["Build-Depends-Indep"][el] = \
-						{(el1.items()[0]) for el1 in section_dict["Build-Depends-Indep"][el]}
+				packages_processing(section_dict["Build-Depends-Indep"])
 
 			section_dict["Standards-Version"] = line["Standards-Version"]
 			section_dict["Homepage"] = line["Homepage"]
 			section_dict["Package"] = line["Package"]
-			section_dict["Architecture"] = line["Architecture"]
-			section_dict["Depends"] = line["Depends"]
 
-			if section_dict["Depends"]:
-				for el in section_dict["Depends"].keys():
-					section_dict["Depends"][el] = \
-						{(el1.items()[0]) for el1 in section_dict["Depends"][el]}
-			section_dict["Description"] = line["Description"]
+			for el in section_dict["Package"].keys():
+				for dep_sect in dep_sects_list:
+					if section_dict["Package"][el][dep_sect]:
+						packages_processing(section_dict["Package"][el][dep_sect])
+
+			#if len(section_dict["Package"]) == 1:
+
+			#else:
+			#	for el in section_dict["Package"].keys():
+
+
+			#section_dict["Architecture"] = line["Architecture"]
+			#section_dict["Depends"] = line["Depends"]
+
+			#if section_dict["Depends"]:
+			#	for el in section_dict["Depends"].keys():
+			#		section_dict["Depends"][el] = \
+			#			{(el1.items()[0]) for el1 in section_dict["Depends"][el]}
+			#section_dict["Description"] = line["Description"]
 
 			build_system = line["Buildsystem"]
 			build_with = line["BuildWith"]
@@ -103,6 +116,10 @@ def main():
 	except KeyboardInterrupt:
 		print '\nThe process was interrupted by the user'
         raise SystemExit
+
+def packages_processing(section):
+	for el in section.keys():
+		section[el] = {(el1.items()[0]) for el1 in section[el]}
 
 def get_build_dependencies(global_req, build_depends, py_file_names = ["setup.py"], update = True):
 	if not build_depends:
@@ -266,10 +283,35 @@ def part_of_package(package, packages):
 
 def load_control(control_file_name = "control"):
 	try:
+		with open(recur_search(control_file_name), 'r') as control_file:
+			cur_package = ""
+			for line in control_file:
+				if not cur_package:
+					for main_sect in main_section_list:
+						if main_sect in line and not section_dict[el]:
+							section_dict[main_sect] = re.sub(":\s+", "",
+								sectTemplate.search(line).group(0))
+				else:
+					for package_sect in package_section_list:
+						if package_sect in line:
+							if package_sect in dep_sects_list:
+								
+							else:
+								
+				if "Package:" in line:
+					cur_package = re.sub(":\s+", "",
+						sectTemplate.search(line).group(0))
+					if section_dict["Package"][cur_package]:
+						cur_package = ""
+
+	except IOError:
+		print "There is no control file!"
+
+	try:
 		with open(recur_search(control_file_name), 'r') as cont_file:
 			for line in cont_file:
 				for el in section_dict.keys():
-					if el not in dep_sects_list and el in line:
+					if el not in build_dep_sects_list and el in line:
 						if not section_dict[el]:
 							section_dict[el] = re.sub(":\s+", "",
 								sectTemplate.search(line).group(0))
@@ -279,29 +321,55 @@ def load_control(control_file_name = "control"):
 def generate_control(control_file_name = "control"):
 	try:
 		with open(recur_search(control_file_name), "w+") as control_file:
-			for el in section_list:
-				if section_dict.has_key(el) and section_dict[el]:
-					control_file.write(el)
-					if el not in dep_sects_list:
-						control_file.write(": {0}\n".format(section_dict[el]))
-						if el == "Homepage":
-							control_file.write("\n")
-					else:
-						control_file.write(":\n")		
-						for key, val in section_dict[el].items():
-							if val:
-								for el2 in val:
-									if el2[0] == "!=":
-										control_file.write(" {0} (<< {1}) | {0} (>> {1})," \
-											.format(key, el2[1]))
-									else:
-										control_file.write(" {0} ({1} {2})," \
-											.format(key, el2[0], el2[1]))
-								control_file.write("\n")
-							else:
-								control_file.write(" {0},\n".format(key))
+			for main_sect in main_section_list:
+				if section_dict.has_key(main_sect) and section_dict[main_sect]:
+					control_file.write("{0}: {1}\n".format(main_sect, section_dict[main_sect]))
+			for pack, value in section_dict["Package"].items():
+				control_file.write("\nPackage: {0}".format(pack))
+				for pack_sect in package_section_list:
+					if value[pack_sect]:
+						if pack_sect in dep_sects_list:
+							control_file.write("{0}:\n".format(pack_sect))
+							for pack_el, pack_val in value[pack_sect]:
+								if pack_el:
+									for dep_el in pack_val:
+										if dep_el[0] == "!=":
+											control_file.write(" {0} (<< {1}) | {0} (>> {1})," \
+												.format(pack_el, dep_el[1]))
+										else:
+											control_file.write(" {0} ({1} {2})," \
+												.format(pack_el, dep_el[0], dep_el[1]))
+						else:
+							control_file.write("{0}: {1}".format(pack_sect, value[pack_sect]))
+				control_file.write("\n")
 	except (IOError, TypeError):
 		print "Error while overwriting control file!"
+
+	#try:
+	#	with open(recur_search(control_file_name), "w+") as control_file:
+	#		for el in section_list:
+	#			if section_dict.has_key(el) and section_dict[el]:
+	#				control_file.write(el)
+	#				if el not in dep_sects_list:
+	#					control_file.write(": {0}\n".format(section_dict[el]))
+	#					if el == "Homepage":
+	#						control_file.write("\n")
+	#				else:
+	#					control_file.write(":\n")		
+	#					for key, val in section_dict[el].items():
+	#						if val:
+	#							for el2 in val:
+	#								if el2[0] == "!=":
+	#									control_file.write(" {0} (<< {1}) | {0} (>> {1})," \
+	#										.format(key, el2[1]))
+	#								else:
+	#									control_file.write(" {0} ({1} {2})," \
+	#										.format(key, el2[0], el2[1]))
+	#							control_file.write("\n")
+	#						else:
+	#							control_file.write(" {0},\n".format(key))
+	#except (IOError, TypeError):
+	#	print "Error while overwriting control file!"
 
 def generate_rules(build_system, build_with, rules_file_name = "rules"):
 	try:
